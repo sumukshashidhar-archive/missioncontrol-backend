eb6ded154fa3ed3cfb5185bc173d57c4fa72e1a9
sumukshashidhar <sumukuuu@gmail.com>
2020-10-18 18:54:53 +0530

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 require("./routes")(app);
 
