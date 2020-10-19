c97f9c56590a8a0f0aa7ba73a86bb140410afb53
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 10:27:54 +0530

    finished the student retrieval route

--- controllers/student_retrieve-microservice.js
+++ controllers/student_retrieve-microservice.js
@@ -1,16 +1,19 @@
-const student = require('./../models/student')
+const student = require("./../models/student");
 module.exports = {
-    
     getStudents: async (classattend, section) => {
         return new Promise(async (resolve, reject) => {
-            student.find({classattend:classattend, section:section}, async (err, obj) => {
-                if(err) {
-                    console.error(err)
+            student.find(
+                { classattend: classattend, section: section },
+                async (err, obj) => {
+                    if (err) {
+                        console.error(err);
+                        resolve(false);
+                    } else {
+                        console.log(obj);
+                        resolve(obj);
+                    }
                 }
-                else {
-                    console.log
-                }
-            })
-        })
-    }   
-}
+            );
+        });
+    },
+};
