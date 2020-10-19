428a63af8cbb7316b99d920b8cac8f1e8f1a4d31
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 10:19:22 +0530

    making the student retrieval service

--- controllers/student_retrieve-microservice.js
+++ controllers/student_retrieve-microservice.js
@@ -1,3 +1,16 @@
+const student = require('./../models/student')
 module.exports = {
     
+    getStudents: async (classattend, section) => {
+        return new Promise(async (resolve, reject) => {
+            student.find({classattend:classattend, section:section}, async (err, obj) => {
+                if(err) {
+                    console.error(err)
+                }
+                else {
+                    console.log
+                }
+            })
+        })
+    }   
 }
