e133853fdd1e3068270df7556e6ff1f2239e30e0
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 12:31:38 +0530

    debug

--- routes/interactTeacher.js
+++ routes/interactTeacher.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
     // this route, if the teacher is authenticated, gets all the students for the particular class
     // first, we check if the teacher is authorized to access this route this way
     const authenticated = auth.authoriseTeacher(req.headers.authorization);
+    console.log(authenticated)
     if (authenticated!==false) {
       // now this means that our user is authorized to access this function
       // let us get her the students of her class!
