fe2002f9d88624b64f00c35a6b10a674478dd878
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 10:15:22 +0530

    teacher auth service for interactions

--- routes/interactTeacher.js
+++ routes/interactTeacher.js
@@ -1,5 +1,16 @@
+const auth = require('./../controllers/authorization-microservice')
 module.exports = (app) => {
     app.get('/api/interaction/getStudents', async (req, res) => {
         // this route, if the teacher is authenticated, gets all the students for the particular class
+        // first, we check if the teacher is authorized to access this route this way
+        const authenticated = auth.authoriseTeacher(token);
+        if(authenticated["status"] === true) {
+            
+        }
+        else {
+            res.status(403).json({
+                "message":"This JWT is invalid / user is not authenticated."
+            })
+        }
     })
 }
