44d66cf2fafb88650438dc65044fa0e22114afa7
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 11:35:11 +0530

    prettier format

--- controllers/points_control-microservice.js
+++ controllers/points_control-microservice.js
@@ -1,32 +1,32 @@
-const student = require("./../models/student")
+const student = require("./../models/student");
 module.exports = {
-
-    addPoints: async (studentid) => {
-        return new Promise(async (resolve, reject) => {
-            student.findOne({ emailID: studentid }, async (err, obj) => {
-                if (err) {
-                    console.error(err)
-                    resolve(false)
+  addPoints: async (studentid) => {
+    return new Promise(async (resolve, reject) => {
+      student.findOne({ emailID: studentid }, async (err, obj) => {
+        if (err) {
+          console.error(err);
+          resolve(false);
+        } else {
+          if (obj !== {} && obj !== null) {
+            student.updateOne(
+              { emailID: studentid },
+              { totalInteractionPoints: obj["totalInteractionPoints"] + 10 },
+              async (err2, obj2) => {
+                if (err2) {
+                  console.error(err);
+                  resolve(false);
+                } else {
+                  console.debug(obj);
+                  console.debug(obj2);
+                  resolve(true);
                 }
-                else {
-                    if (obj !== {} && obj !== null) {
-                        student.updateOne({ emailID: studentid }, { totalInteractionPoints: obj["totalInteractionPoints"] + 10 }, async (err2, obj2) => {
-                            if (err2) {
-                                console.error(err)
-                                resolve(false)
-                            }
-                            else {
-                                console.debug(obj)
-                                console.debug(obj2)
-                                resolve(true)
-                            }
-                        })
-                    }
-                    else {
-                        resolve(false)
-                    }
-                }
-            })
-        })
-    }
-}
+              }
+            );
+          } else {
+            resolve(false);
+          }
+        }
+      });
+    });
+  },
+};
--- models/assingment.js
+++ models/assingment.js
@@ -1,12 +1,12 @@
 var mongoose = require("mongoose");
 
 var assignmentSchema = new mongoose.Schema({
-    class_assigned: Number,
-    section: String,
-    teacher_assigned_by: String,
-    dueDate: Number,
-    extensionPurchasedBy: Array, 
-    newDueDate: Array,
+  class_assigned: Number,
+  section: String,
+  teacher_assigned_by: String,
+  dueDate: Number,
+  extensionPurchasedBy: Array,
+  newDueDate: Array,
 });
 
 module.exports = mongoose.model("assignment", assignmentSchema);
