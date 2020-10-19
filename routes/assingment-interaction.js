b659e0fbf35328455639fa7516da6bb37afd8b17
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 21:04:26 +0530

    fleshing out the routes

--- routes/assingment-interaction.js
+++ routes/assingment-interaction.js
@@ -6,25 +6,55 @@ module.exports = (app) => {
     */
 
     app.get('/api/assignments/student/getAssignemts', async(req, res) => {
+        const authenticated = await auth.authoriseStudent(
+            req.headers.authorization
+          );
 
+          if(authenticated!==false) {
+
+          } 
     })
     /*
     Allows a student to submit a particular assignment
     */
     app.post('/api/assignment/student/submitAssignment', async(req, res) => {
-        
+        const authenticated = await auth.authoriseStudent(
+            req.headers.authorization
+          );
+
+          if(authenticated!==false) {
+              
+        } 
     })
 
     app.post('/api/assignments/students/requestExtension', async(req, res) => {
-        
+        const authenticated = await auth.authoriseStudent(
+            req.headers.authorization
+          );
+
+          if(authenticated!==false) {
+              
+        } 
     })
 
     app.get('/api/assignments/teacher/getAssignments', async(req, res) => {
+        const authenticated = await auth.authoriseTeacher(
+            req.headers.authorization
+          );
 
+          if(authenticated!==false) {
+              
+        } 
     })
 
     app.post('/api/assignments/teacher/makeAssignmnt', async(req, res) => {
+        const authenticated = await auth.authoriseTeacher(
+            req.headers.authorization
+          );
 
+          if(authenticated!==false) {
+              
+        } 
     })
 
 }
