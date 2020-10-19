c58660f11f4699c1703c5576b1cbfb62571d45c2
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 12:47:54 +0530

    fixed the final bug

--- controllers/authorization-microservice.js
+++ controllers/authorization-microservice.js
@@ -2,7 +2,6 @@ const jwms = require("./jwt-microservice")
 module.exports = {
   authoriseTeacher: async (token) => {
     const teach = await jwms.total_verification(token)
-    console.log(teach)
     if(teach!==false && teach["role"]==="teacher"){
       return teach
     }
--- controllers/student_retrieve-microservice.js
+++ controllers/student_retrieve-microservice.js
@@ -2,8 +2,9 @@ const student = require("./../models/student");
 module.exports = {
   getStudents: async (classattend, section) => {
     return new Promise(async (resolve, reject) => {
+      console.log(classattend, section)
       student.find(
-        { classattend: classattend, section: section },
+        { student_class: classattend, student_section: section },
         async (err, obj) => {
           if (err) {
             console.error(err);
