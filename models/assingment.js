aaf000d4defedd18c437b500f7888457170c833b
sumukshashidhar <sumukuuu@gmail.com>
2020-10-19 21:13:39 +0530

    finsihed making assingments

--- models/assingment.js
+++ models/assingment.js
@@ -4,13 +4,14 @@ var assignmentSchema = new mongoose.Schema({
   class_assigned: Number,
   section: String,
   teacher_assigned_by: String,
+  teacher_name: String,
   dueDate: Number,
   assignemtName: String,
   assignmentLink: String,
-  extensionPurchasedBy: Array,
-  newDueDate: Array,
-  submittedStudents: Array,
-  submittedStudentsLink: Array,
+  extensionPurchasedBy: {type:Array, default:[]},
+  newDueDate: {type:Array, default:[]},
+  submittedStudents: {type:Array, default:[]},
+  submittedStudentsLink: {type:Array, default:[]}
 });
 
 module.exports = mongoose.model("assignment", assignmentSchema);
