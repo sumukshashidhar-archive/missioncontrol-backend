e71434521618bce3473cb18b452b8a6eec786985
sumukshashidhar <sumukuuu@gmail.com>
2020-10-21 10:24:04 +0530

    lot of registration changes

--- controllers/registration-microservice.js
+++ controllers/registration-microservice.js
@@ -2,12 +2,34 @@ const logger = require("./../config/logger")
 const registration_service = require("./registration-service")
 const account_creation_microservice = require("./account_creation-microservice")
 module.exports = {
-    register_user: async function(username, password, role) {
+    register_user: async function(username, password, role, name, grade, section) {
         return new Promise(async function(resolve, reject) {
             // first, we create the "user"
             const response = await registration_service.makeUser(username, password, role)
             if(response) {
                 // meaning that we were able to create the user
+                // check the roles first
+                if (role === 'student') {
+                    const account_response = await account_creation_microservice.makeStudentAccount(username, name, grade, section)
+                    if(account_response) {
+                        // means that the account was successfully crated
+                        resolve({
+                            "status":true,
+                            "object":true
+                        })
+                    }
+                }
+                else {
+                    // means teacher automatically
+                    const account_response = await account_creation_microservice.makeTeacherAccount(username, name, grade, section)
+                    if(account_response) {
+                        // means that the account was successfully crated
+                        resolve({
+                            "status":true,
+                            "object":true
+                        })
+                    }
+                }
             }
             else {
                 // meaning user was not created, so account should not be created
--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,8 @@ const teacher = require("../models/teacher");
 /*
     SERVICES
  */
-const token_microservice = require("./../controllers/jwt-microservice");
-const password_module = require("../controllers/auth-microservice");
-const registration_microservice = require("./../controllers/registration-service");
-const account_creation_microservice = require("./../controllers/account_creation-microservice");
 const login_microservice = require("./../controllers/login-microservice")
-
+const registration_microservice = require("./../controllers/registration-microservice")
 const logger = require("./../config/logger")
 
 module.exports = (app) => {
@@ -34,98 +30,7 @@ module.exports = (app) => {
         }
     }),
         app.post("/register", async function (req, res) {
-            if (
-                req.body.creation_password === process.env.ADMIN_CREATION_SECRET &&
-                req.body.role === "admin"
-            ) {
-                const response = await registration_microservice.makeUser(
-                    req.body.username,
-                    req.body.password,
-                    req.body.role
-                );
-                if (response) {
-                    //meaning the admin user creation succeeded, now we need to look to creating the actual admin user as well.
-                    res.json({
-                        message: "Successfully Created the User",
-                    });
-                    console.info("Created the user");
-                } else {
-                    res.status(409).json({
-                        message: "Did not create the user",
-                    });
-                }
-            } else if (
-                (req.body.creation_password === process.env.ADMIN_CREATION_SECRET ||
-                    req.body.creation_password === process.env.DEVICE_CREATION_SECRET) &&
-                req.body.role === "teacher"
-            ) {
-                // this is for registration of regular users
-                const response = await registration_microservice.makeUser(
-                    req.body.username,
-                    req.body.password,
-                    req.body.role
-                );
-                if (response) {
-                    // teacher USER is created, now we create the actual teacher account!
-                    var responseAccCreate = account_creation_microservice.makeTeacherAccount(
-                        req.body.username,
-                        req.body.name,
-                        12,
-                        req.body.section
-                    );
-                    if (responseAccCreate) {
-                        // this means everything went well
-                        res.json({
-                            message: "Successfully Created the User",
-                        });
-                    } else {
-                        res.json({
-                            message: "Something went horribly wrong",
-                        });
-                    }
-
-                    console.info("Created the user");
-                } else {
-                    res.status(409).json({
-                        message: "Did not create the user as the user already exists.",
-                    });
-                }
-            } else if (req.body.role === "student") {
-                // this is for registration of regular users
-                const response = await registration_microservice.makeUser(
-                    req.body.username,
-                    req.body.password,
-                    req.body.role
-                );
-                if (response) {
-                    var responseAccCreate = account_creation_microservice.makeStudentAccount(
-                        req.body.username,
-                        req.body.name,
-                        12,
-                        req.body.section
-                    );
-                    if (responseAccCreate) {
-                        res.json({
-                            message: "Successfully Created the User",
-                        });
-                    } else {
-                        res.json({
-                            message: "Something went horribly wrong",
-                        });
-                    }
-                    console.info("Created the user");
-                } else {
-                    res.status(409).json({
-                        message: "Did not create the user as the user already exists.",
-                    });
-                }
-            } else {
-                res.status(403).json({
-                    message:
-                        "There was no creation password specified for the current user or the creation password for a different role or the creation password was wrong",
-                });
-            }
-
-            console.log(req.body);
+            logger.debug(`The username is ${req.body.username} password is ${req.body.password} name is ${req.body.name} class is ${req.body.grade} section is ${req.body.section}`)
+            const response
         });
 };
